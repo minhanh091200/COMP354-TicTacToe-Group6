76b75f80bc265dc08f1c6b9cf5740b9a10f8f930
Layla <86987909+layla-m@users.noreply.github.com>
2024-07-15 07:30:16 -0400

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -1,17 +1,16 @@
-
 let instuctions = document.querySelector(".popup-container");
 let Player1Buttun = document.querySelector(".player1-btn");
 let Player2Buttun = document.querySelector(".player2-btn");
 let customContainer = document.querySelector(".container-custom");
 
 // adding functionalities to the instructions container
-Player1Buttun.addEventListener("click", () =>{
+Player1Buttun.addEventListener("click", () => {
     singlePlayer();
     instuctions.style.display = "none";
     customContainer.style.display = "flex";
-})
-Player2Buttun.addEventListener("click", () =>{
+});
+Player2Buttun.addEventListener("click", () => {
     twoPlayer();
     instuctions.style.display = "none";
     customContainer.style.display = "flex";
-})
+});
